f38fa75c50801f28e82642d94ea4760c357c9f03
tawan2556 <134558376+tawan2556@users.noreply.github.com>
2023-05-25 05:52:49 +0700

    Add files via upload

---
+++ js/tawan.js
@@ -0,0 +1,4 @@
+function setting(){
+    document.querySelector('.copyright').innerHTML = "Copyright  @ MVT_Tawan Created by <a href="https://www.facebook.com/TawanAlongkorn"
+        target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a>"
+    }
---
+++ js/tawanFt.js
@@ -0,0 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" />
+function setFooter(){
+    document.getElementById('copyright').innerHTML = '<div class="text-center"><a href="https://www.facebook.com/TawanAlongkorn" target="_blank"><i class="fa-brands fa-facebook fa-2x"></i></a></div><center style="font-size:12px;">Vending Machine &copy; 2023 <a style="color:gray;text-decoration:none" href="https://store-cms.vdm.cpall.co.th/login" target="_blank"> Contact</a></center>'
+  }
